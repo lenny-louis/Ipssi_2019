331fea5d8251154f400c8dadeb004674d162698c
lenny-louis <louislennyy@gmail.com>
2019-12-06 12:43:43 +0100

    ok2

--- exam3/tree.py
+++ exam3/tree.py
@@ -1,17 +1,4 @@
-"""import sys
 
-N = int(sys.argv[1])
-
-def fn(n):
-    if n != 0:
-        print(n*' '+((N-n)*2+1)*'x')
-        fn(n-1)
-    else:
-        #if N<=Y
-        print(N*' '+'x')
-fn(int(sys.argv[1]))"""
-
-####
 import sys
 
 N = int(sys.argv[1])
@@ -23,7 +10,7 @@ def fn(n):
         fn(n-1)
     else:
         if ( Y <= N ):
-            print(N*' '+'xxx')
+            print(N*' '+'xx')
         else:
             print(N*' '+'x')
 fn(int(sys.argv[1]))
