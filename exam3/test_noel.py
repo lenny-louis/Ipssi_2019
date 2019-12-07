523cb94b8fd69390c3b2fe5d33d74ee5c218a568
lenny-louis <louislennyy@gmail.com>
2019-12-07 21:19:45 +0100

    script work

---
+++ exam3/test_noel.py
@@ -0,0 +1,7 @@
+import sys
+import calendar
+from datetime import datetime, timedelta
+from tree import show_tree
+from datetime import date
+from noel import show_noel
+print(show_noel(sys.argv))
---
+++ exam3/test_tree.py
@@ -0,0 +1,3 @@
+import sys
+from tree import show_tree
+print(show_tree(int(sys.argv[1])))
