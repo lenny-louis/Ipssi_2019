7c451451d55c395bb25855e7e4237baec1a3047a
lenny-louis <louislennyy@gmail.com>
2019-12-07 11:54:54 +0100

    commit 07122019

---
+++ exam3/sapin_noel.py
@@ -0,0 +1,20 @@
+import random as r
+import sys
+#get input
+#n = int(input())
+n = int(sys.argv[1])
+#make the top of the tree
+print(n*' '+'x')
+#this loop draws the entire tree's body--
+#--------------------------------
+for i in range(1,n):
+ #s is stored as a string of length i*2 with * and ~ in random positions, this is used for the body of the tree
+ s = ''.join(r.choice(['x','o',r.choice(['x','o'])]) for i in range(i*2))
+ #string sl and sr holds the random string for the left and right side boundary of the tree's body
+ sl = r.choice(['x'])
+ sr = r.choice(['o'])
+ #print all of 'em at last
+ print((n-i)*' '+sl+s+sr)
+#print the trunk
+
+print((n-1)*' ' + "xx")
