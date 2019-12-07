e5ec3323301eb2434b3c43f92878137e03621c26
lenny-louis <louislennyy@gmail.com>
2019-12-07 21:25:29 +0100

    script work

--- exam3/sla.py
+++ exam3/sla.py
@@ -5,8 +5,8 @@ import os
 def show_sla(d):
     ind=3600*24*365.25*(1-(d/100))
     #99.9 = 31557, convertir
-    res = time.strftime('%Hh %Mm %Ss', time.gmtime(ind))
-    return res
+    resultatcalcul = time.strftime('%Hh %Mm %Ss', time.gmtime(ind))
+    return resultatcalcul
 
 if __name__ == "__main__":
     print(show_sla(float(sys.argv[1])))
