70188a01b0469ed575c683a4801a9f419db955cb
lenny-louis <louislennyy@gmail.com>
2019-12-07 21:29:51 +0100

    script work

--- ex04/logs.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from datetime import datetime
-now = datetime.now()
-date_time = now.strftime("%m/%d/%Y %H:%M:%S")
-
-fichier = open("python.log", "a")
-fichier.write(str(date_time))
-fichier.close()
--- ex10/read_json.py
+++
@@ -1,7 +0,0 @@
-import json
-from pprint import pprint
-
-with open('students.json') as f:
-    data = json.load(f)
-
-pprint(data)
