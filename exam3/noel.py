c8b024cab38f1d007a623a4a4dd672fc24ada791
lenny-louis <louislennyy@gmail.com>
2019-12-07 16:51:17 +0100

    commit 07122019

--- exam3/noel.py
+++ exam3/noel.py
@@ -15,28 +15,29 @@ d1 = today.strftime("%d")
 print (int(datetime.strftime(modified_date, "%d")) - int(today.strftime("%d")), "days before christmas","\n","\n") 
 
 
-def show_noel(allArg):
-    if len(allArg) > 1:
-        date_time_str = allArg[1]
-        theDate = datetime.strptime(date_time_str, '%Y-%m-%d')
+def show_noel(n):
+    if len(n) > 1:
+        date_time_str = n[1]
+        datedata = datetime.strptime(date_time_str, '%Y-%m-%d')
         
     else:
-        theDate = datetime.now()
+        datedata = datetime.now()
         
         
        
 
-    c = calendar.TextCalendar(calendar.MONDAY)
-    firstCalendar = c.formatmonth(theDate.year, theDate.month, 5, 1)
-    firstCalendar = firstCalendar.replace(str(theDate.day), "("+str(theDate.day)+")", 1)
+    var = calendar.TextCalendar(calendar.MONDAY)
+    firstCalendar = var.formatmonth(datedata.year, datedata.month, 5, 1)
+    firstCalendar = firstCalendar.replace(str(datedata.day), "("+str(datedata.day)+")", 1)
     
-    c = calendar.TextCalendar(calendar.MONDAY)
-    secondCalendar = c.formatmonth(theDate.year, 12, 5, 1)
-    secondCalendar = secondCalendar.replace(" 25 ", "[25]", 1)
+    var = calendar.TextCalendar(calendar.MONDAY)
+    next = var.formatmonth(datedata.year, 12, 5, 1)
+    next = next.replace(" 25 ", "[25]", 1)
 
-    if theDate.strftime('%Y-%m-%d') == str(theDate.year)+"-12-25":
+    if datedata.strftime('%Y-%m-%d') == datetime.strptime(s, "%Y/%m/%d"):
         return show_tree(10)
     else:
-        return firstCalendar+secondCalendar
+        return firstCalendar+next
+
 if __name__ == "__main__":
     print(show_noel(sys.argv))
