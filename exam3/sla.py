f3f42421e027b963863738db282c9e5c38c8fac1
lenny-louis <57408297+lenny-louis@users.noreply.github.com>
2019-12-15 21:35:10 +0100

    Update sla.py

--- exam3/sla.py
+++ exam3/sla.py
@@ -1,12 +1,15 @@
 import sys
 import time
-import os
+from math import floor
 
-def show_sla(d):
-    ind=3600*24*365.25*(1-(d/100))
-    #99.9 = 31557, convertir
-    resultatcalcul = time.strftime('%Hh %Mm %Ss', time.gmtime(ind))
-    return resultatcalcul
+def show_sla(disponibilite):
+    indispo=3600*24*365.25*(1-(disponibilite/100))
+    h = (int(indispo // 3600))
+    m = indispo % 3600 // 60
+    s = indispo % 3600 % 60 // 1
+
+    return ("{}h {}m {}s".format(h, floor(m),s))
+
 
 if __name__ == "__main__":
     print(show_sla(float(sys.argv[1])))
